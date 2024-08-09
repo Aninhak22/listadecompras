79e28841eb6c30c7472d28babbb937672352c2b2
Aninhak22 <anakellymenegus@gmail.com>
2024-08-09 08:34:59 -0300

    tiramos o comentário do senhor Heitor

--- listmarket/lib/main.dart
+++ listmarket/lib/main.dart
@@ -11,7 +11,7 @@ class MeuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ListBarket',
+      title: 'ListMarket',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
